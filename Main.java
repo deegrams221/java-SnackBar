c32b33c5cd42ab117ed3da6b65f2ac7609842cec
Diana Grams <deegrams221@gmail.com>
2019-09-30 13:50:15 -0600

    project setup, started on instantiating

---
+++ Main.java
@@ -0,0 +1,20 @@
+package java-SnackBar;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Customer c1 = new Customer("Jane", 45.25, c1.id);
+		Customer c2 = new Customer("Bob", 33.14, c2.id);
+
+		VendingMachine vm1 = new VendingMachine("Food", vm1.id);
+		VendingMachine vm2 = new VendingMachine("Drink", vm2.id);
+		VendingMachine vm3 = new VendingMachine("Office", vm3.id);
+
+		Snack s1 = new Snack("name", quantity, cost, vm.id, s1.id);
+		Snack s1 = new Snack("name", quantity, cost, vm.id, s1.id);
+		Snack s1 = new Snack("name", quantity, cost, vm.id, s1.id);
+		Snack s1 = new Snack("name", quantity, cost, vm.id, s1.id);
+		Snack s1 = new Snack("name", quantity, cost, vm.id, s1.id);	
+	}
+}
